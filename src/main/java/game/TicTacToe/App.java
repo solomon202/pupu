aaf0975ddf67628a737b23208f861dc7f4521842
Your Name <you@example.com>
2026-01-07 01:31:19 +0300

    two

--- src/main/java/game/TicTacToe/App.java
+++ src/main/java/game/TicTacToe/App.java
@@ -3,5 +3,8 @@ package game.TicTacToe;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    Game game = new Game();
+    game.start();
+   
   }
 }
---
+++ src/main/java/game/TicTacToe/Game.java
@@ -0,0 +1,18 @@
+package game.TicTacToe;
+
+import javax.swing.JFrame;
+
+public class Game {
+
+	public void start() {
+		JFrame frame = new JFrame("FrameDemo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setSize(350,350);
+		frame.setVisible(true);
+		
+	}
+
+
+
+}
